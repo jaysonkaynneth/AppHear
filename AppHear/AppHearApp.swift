3023f9bcaf41ca781212b7e35a1dcc92908004f2
Jason Kenneth <jason.hauw@binus.ac.id>
2022-10-11 15:07:10 +0700

    Initial Commit

---
+++ AppHear/AppHearApp.swift
@@ -0,0 +1,20 @@
+//
+//  AppHearApp.swift
+//  AppHear
+//
+//  Created by Jason Kenneth on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AppHearApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
