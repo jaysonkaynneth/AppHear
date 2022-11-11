b8471affde88648e34550d3d995f27ccf4332e39
Piterong <piterwongso6@gmail.com>
2022-11-11 19:15:32 +0700

    Finished dictionary feature

---
+++ AppHear/Extensions/String.swift
@@ -0,0 +1,65 @@
+//
+//  String.swift
+//  AppHear
+//
+//  Created by Piter Wongso on 11/11/22.
+//
+
+import Foundation
+
+public extension String {
+    
+    func between(_ left: String, _ right: String) -> String? {
+        guard
+            let leftRange = range(of: left), let rightRange = range(of: right, options: .backwards)
+                , leftRange.upperBound <= rightRange.lowerBound
+        else { return nil }
+        
+        let sub = self[leftRange.upperBound...]
+        let closestToLeftRange = sub.range(of: right)!
+        return String(sub[..<closestToLeftRange.lowerBound])
+    }
+    
+    var length: Int {
+        get {
+            return self.count
+        }
+    }
+    
+    func substring(to : Int) -> String {
+        let toIndex = self.index(self.startIndex, offsetBy: to)
+        return String(self[...toIndex])
+    }
+    
+    func substring(from : Int) -> String {
+        let fromIndex = self.index(self.startIndex, offsetBy: from)
+        return String(self[fromIndex...])
+    }
+    
+    func substring(_ r: Range<Int>) -> String {
+        let fromIndex = self.index(self.startIndex, offsetBy: r.lowerBound)
+        let toIndex = self.index(self.startIndex, offsetBy: r.upperBound)
+        let indexRange = Range<String.Index>(uncheckedBounds: (lower: fromIndex, upper: toIndex))
+        return String(self[indexRange])
+    }
+    
+    func character(_ at: Int) -> Character {
+        return self[self.index(self.startIndex, offsetBy: at)]
+    }
+    func firstIndexOfCharacter(_ c: Character) -> Int? {
+        guard let index = range(of: String(c))?.lowerBound else
+        { return nil }
+        return distance(from: startIndex, to: index)
+    }
+    
+    func lastIndexOfCharacter(_ c: Character) -> Int? {
+        guard let index = range(of: String(c), options: .backwards)?.lowerBound else
+        { return nil }
+        return distance(from: startIndex, to: index)
+    }
+    func trimTrailingPunctuation() -> String {
+        return self.trimmingCharacters(in: .whitespacesAndNewlines)
+            .trimmingCharacters(in: .punctuationCharacters)
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
